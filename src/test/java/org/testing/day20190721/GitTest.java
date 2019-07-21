49e6e8ecd6a5cafd6eceb34a032d477e35587d37
gblbj <2602251350@qq.com>
2019-07-21 08:03:42 +0800

    git新提交测试

---
+++ src/test/java/org/testing/day20190721/GitTest.java
@@ -0,0 +1,10 @@
+package org.testing.day20190721;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+    @Test
+    public void Test(){
+        System.out.println("git测试");
+    }
+}
